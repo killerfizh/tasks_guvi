8566d748634e035f42999d3b80c8c9bccd468659
Krishna Kompalli <31897761+killerfizh@users.noreply.github.com>
2020-04-05 20:49:21 +0530

    Create vehicle_spec.js

---
+++ vehicle_spec.js
@@ -0,0 +1,52 @@
+var Dimensions = {
+  Overall_length : "3,500 mm",
+  Overall_width : "1,600 mm",
+  Overall_height : "1,490 mm",
+  Wheelbase : "2,360 mm",
+  Track_width : {
+    Front : "1,405 mm",
+    Rear : "1,400 mm",
+  },
+  Minimum_turning_radius : "4.5 m",
+  Minimum_ground_clearance : "170 mm",
+  }
+
+var Capacities = {
+  Seating_capacity : "5 persons",
+  Fuel_tank_capacity : "35 litres"
+}
+
+var Chassis = {
+  Steering : "Rack & Pinion, Power assisted",
+  Brakes : {
+    Front : "Ventilated discs",
+    Rear : "Drums"
+  },
+  Suspension : {
+    Front : "MacPherson strut & coil spring",
+    Rear : "Isolated trailing link & coil spring"
+  },
+  Shock_absorbers : "Gas Filled",
+  Tyre : "155/80R13 (tubeless)"
+}
+
+var Transmission = {
+  Type : "5-speed MT"
+}
+
+var Engine = {
+  Type:"KB-Series",
+  Number_of_cylinder:"3",
+   Number_of_valves:"12",
+   Capacity:"998 cc/cm^3",
+   Bore_x_store:"73.0X79.5 mm",
+   Compression_ration:"10:1",
+   Maximumn_Power:"67/6,200",
+   Maximum_torque:"90/3,500 Nm/rpm",
+   Fuel_Distribution:"Multipoint Injection",
+ }
+
+ var Weight = {
+   kerb_weight_min:"860-880",
+   Gross_vehicle_Weight:"1,320",
+};
