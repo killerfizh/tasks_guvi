3bdf384770103467ed5bd1d4479288ae20c33fe0
Krishna Kompalli <31897761+killerfizh@users.noreply.github.com>
2020-04-05 22:35:00 +0530

    Update typeof.js

--- typeof.js
+++ typeof.js
@@ -1,13 +1,18 @@
-//Data Types
-var a = "04"
-console.log("'04' " + typeof a)
-var b = 04
-console.log("04 " + typeof b)
-var c = true
-console.log("true " + typeof c)
-var cars = ["Saab", "Volvo", "BMW"]
-console.log("cars " + typeof cars)
-var person = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
-console.log("person " + typeof person )
-var none;
-console.log("none " + typeof none)
+var person = {
+	first_name : 'john',
+	last_name : 'jacob',
+};
+var task = [
+	typeof 1,
+	typeof null,
+	typeof undefined,
+	typeof 0.50,
+	typeof 'k',
+	typeof 'hello',
+	typeof person,
+	];
+for (let type of task)
+{
+	console.log(type);
+}
+console.log(typeof task);
