a10e71cf094354acd078e3d361aec358e9f76439
Krishna Kompalli <31897761+killerfizh@users.noreply.github.com>
2020-04-05 22:38:35 +0530

    Create rand_add.js

---
+++ rand_add.js
@@ -0,0 +1,10 @@
+Math.floor(Math.random() * 11) //function to generate a random whole number between 0 to 10
+
+// fuction to get two parameters and return a sum of it
+function Add(a,b){
+  console.log(a+b)
+}
+a = parseInt(prompt("enter a num"))
+b = parseInt(prompt("enter another num"))
+Add(a,b)
+
