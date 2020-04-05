def637cf55858af4b3d888fcaf5b050d576d2080
Krishna Kompalli <31897761+killerfizh@users.noreply.github.com>
2020-04-05 19:58:14 +0530

    Create typeof.js

---
+++ typeof.js
@@ -0,0 +1,13 @@
+//Data Types
+var a = "04"
+console.log("'04' " + typeof a)
+var b = 04
+console.log("04 " + typeof b)
+var c = true
+console.log("true " + typeof c)
+var cars = ["Saab", "Volvo", "BMW"]
+console.log("cars " + typeof cars)
+var person = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
+console.log("person " + typeof person )
+var none;
+console.log("none " + typeof none)
