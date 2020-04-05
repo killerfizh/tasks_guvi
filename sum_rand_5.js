5e7623ac579b88bcd2294327ff678555a671eea7
Krishna Kompalli <31897761+killerfizh@users.noreply.github.com>
2020-04-06 00:10:58 +0530

    Create sum_rand_5.js

---
+++ sum_rand_5.js
@@ -0,0 +1,30 @@
+//function to return sum
+function Adding(a,b)
+{
+  return a+b
+}
+p = parseInt(window.prompt())
+q = parseInt(window.prompt())
+Adding(p,q)
+
+
+
+
+// function to generate a random number
+
+function Rand()
+{
+  console.log(Math.floor(Math.random()*10))
+}
+Rand()
+
+
+
+
+// adding with a random number
+function Add(a,b){
+  return a+b
+}
+p = parseInt(window.prompt("enter a number"))
+q = Math.floor(Math.random())
+console.log(Add(p,q))
